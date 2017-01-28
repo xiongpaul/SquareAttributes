27aa338d4cab8a35b20392815d5ae432bde512e5
Paul Xiong <paul.nost.xiong@gmail.com>
2017-01-27 21:52:01 -0500

    completed Square class

---
+++ PXAssignment1/Square.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PXAssignment1
+{    
+    public class Square
+    {
+        private int side;
+
+        //default constructor
+        public Square()
+        {
+            
+        }
+
+        //non-default constructor
+        public Square(int side)
+        {
+            this.side = side;
+        }
+
+        //method to get a side;
+        public int GetSide()
+        {
+            return this.side;
+        }
+
+        //method to change the side value
+        public int ChangeSide(int side)
+        {
+            this.side = side;
+            return side;
+        }
+
+        //method to get perimeter
+        public int GetPerimeter()
+        {
+            int perimeter = side * 4;
+            return perimeter;
+        }
+
+        //method to get area of square
+        public int GetArea()
+        {
+            int area = side ^ 2;
+            return area;
+        }
+
+    }
+}
